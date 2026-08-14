72d6c83b243734b5ae1552f1e9e3369ad9e4e4dc
androidacy-user <opensource@androidacy.com>
2026-08-14 03:19:38 +0000

    build: compile with kotlin 2.4.10
    AGP 9's built-in Kotlin follows the highest kotlin-gradle-plugin on the
    buildscript classpath, so declaring it unapplied raises it from 2.2.10.
    
    Signed-off-by: androidacy-user <opensource@androidacy.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.library") version "9.3.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.10" apply false
     id("org.jetbrains.dokka") version "2.2.0" apply false
     id("org.jetbrains.dokka-javadoc") version "2.2.0" apply false
 }
